ff08ffd42d63f32df8b79ee0e51044668292f397
Mauro García Barro <m.gbarro@udc.es>
2023-10-12 21:04:33 +0200

    Datos formateados para index

--- practicaRIWS/spiders/wikidexCSP.py
+++ practicaRIWS/spiders/wikidexCSP.py
@@ -34,12 +34,18 @@ class wikidexCSP(CrawlSpider):
         tipo = response.css("table.datos.resalto td a::attr(title)").re(r"(.*Tipo.*)")
 
         peso = response.css("table.datos.resalto td::text").re(r"(.*kg.*)")[0]
+        peso = peso.replace(' kg', '')
+        peso = peso.replace(',', '.')
+        peso = float(peso)
 
         altura = response.css("table.datos.resalto td::text").re(r"(.*\d m.*)")[0]
+        altura = altura.replace(' m', '')
+        altura = altura.replace(',', '.')
+        altura = float(altura)
 
         sexos = response.css("table.datos.resalto td::text").re(r"(.*\%.*)")
 
-        if(len(sexos) == 0):
+        if(len(sexos) == 0 or sexos == '?%'):
             sexomacho = '0%'
             sexohembra = '0%'
 
@@ -47,14 +53,22 @@ class wikidexCSP(CrawlSpider):
             sexomacho = sexos[0]
             sexohembra = sexos[1]
 
-        imagenpokemon = 'https://www.wikidex.net' + response.css("a::attr(href)").re_first(r"/wiki/Archivo:.+")
+        sexomacho = sexomacho.replace('%', '')
+        sexomacho = sexomacho.replace(',', '.')
+        sexomacho = float(sexomacho)
+
+        sexohembra = sexohembra.replace('%', '')
+        sexohembra = sexohembra.replace(',', '.')
+        sexohembra = float(sexohembra)
+
+        imagenpokemon = 'https://www.wikidex.net' + response.css("div.imagen a::attr(href)").re_first(r"/wiki/Archivo:.+")
 
         yield {
             'Nombre': nombre,
             'Generación': generacion,
             'Tipo': tipo,
-            'Peso': peso,
-            'Altura': altura,
+            'Peso(kg)': peso,
+            'Altura(m)': altura,
             'ProbMacho': sexomacho,
             'ProbHembra': sexohembra,
             'Imagen': imagenpokemon
